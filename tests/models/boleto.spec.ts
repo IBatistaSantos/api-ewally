d6ab6fc9ca8c33d14cc45b83ac67f456ba3a5339
Israel Batista <israelbatiista19@gmail.com>
2022-04-25 11:04:03 -0300

    remove test with Date

--- tests/models/boleto.spec.ts
+++ tests/models/boleto.spec.ts
@@ -7,11 +7,6 @@ describe('Boleto', () => {
   beforeAll(() => {
     barCode = '00193373700000001000500940144816060680935031';
   });
-  it('should expirationDate returns a date', () => {
-    const dateReturned = new Date('2017-12-11T02:00:00.000Z');
-    const result = Boleto.expirationDate(barCode);
-    expect(result).toEqual(dateReturned);
-  });
 
   it('should value returns a number', () => {
     const result = Boleto.value(barCode);
